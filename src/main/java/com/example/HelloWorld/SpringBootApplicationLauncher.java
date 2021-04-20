ab4bcb30afabcf5aee45ec3d478d5b85f43b40b1
anthonyredmond <kushkurosaki@icloud.com>
2021-04-20 17:47:20 -0500

    hello commit

---
+++ src/main/java/com/example/HelloWorld/SpringBootApplicationLauncher.java
@@ -0,0 +1,20 @@
+package com.example.HelloWorld;
+
+import org.springframework.boot.SpringApplication;
+
+
+
+
+        import org.springframework.boot.SpringApplication;
+
+/**
+ * This Launcher for the spring boot application.
+ *
+ * @author manoj.bardhan
+ *
+ */
+public class SpringBootApplicationLauncher {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldController.class, args);
+    }
+}
